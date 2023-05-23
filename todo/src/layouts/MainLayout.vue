f0b63339d1fc333e09642fd6bd498f8b4860095b
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2023-05-22 21:32:00 -0300

    Image and avatar

--- todo/src/layouts/MainLayout.vue
+++ todo/src/layouts/MainLayout.vue
@@ -14,31 +14,41 @@
       </q-toolbar>
       <div class="q-px-lg q-pt-xl q-mb-md">
       <div class ="text-h3"> Todo</div>
-      <div class="text-subtitle1">Monaday 4 november </div>
+      <div class="text-subtitle1">{{ todaysDate }}</div>
       </div>
-      <q-img src="/todo/src/penguin-g7550bab4f_1280.jpg" class="header-image absolute-top"/>
-
+      <q-img src="https://cdn.pixabay.com/photo/2023/05/07/20/44/woman-7977101_1280.jpg" class="header-image absolute-top"/>
   </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+  <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="300"
+      >
+        <q-scroll-area style="height: calc(100% - 188px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list"/>
+              </q-item-section>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.pixabay.com/photo/2023/05/07/20/44/woman-7977101_1280.jpg" style="height: 165px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://www.petlove.com.br/images/breeds/193223/profile/original/golden_retriever-p.jpg?1532539102">
+            </q-avatar>
+            <div class="text-weight-bold">Ellen Carine Bonafin Marques </div>
+            <div>@ellencarine@outklock</div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -46,8 +56,11 @@
   </q-layout>
 </template>
 
+
+
 <script>
-import { defineComponent, ref } from 'vue'
+
+import { computed, defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
@@ -95,6 +108,7 @@ const linksList = [
   }
 ]
 
+import { date } from 'quasar'
 export default defineComponent({
   name: 'MainLayout',
 
@@ -109,16 +123,26 @@ export default defineComponent({
       essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+        leftDrawerOpen.value = false
       }
+}
+  },
+  computed:{
+    todaysDate(){
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
     }
-  }
+}
 })
+// export default {
+
 </script>
 
 <style lang="scss">
- .header-image{
-  height: 100%;
-  z-index: -1;
- }
+  .header-image{
+    height: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter:grayscale(100%);
+  }
 </style>
